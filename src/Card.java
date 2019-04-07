8b85d4d1be8c83897c1cbfc9cf99c309aeb2544e
y26born <y26born@siena.edu>
2019-04-07 13:23:25 -0400

    the card class is created

---
+++ src/Card.java
@@ -0,0 +1,12 @@
+
+/**
+ * The Card class for ticket to ride - New York.
+ *
+ * @authors Michael Lamb, Justin Largo, Leon Griffiths, Jennifer LeClair, Yousuf
+ *      Borna
+ * @version 1.0
+ */
+public class Card
+{
+    
+}
